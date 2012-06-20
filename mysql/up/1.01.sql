ca22f5b2b3c9062ad03bccab15ca0e7be89c7e06
dennis <dennis@dennis-laptop.(none)>
2012-06-20 13:50:42 +0200

    update old tutorial

--- mysql/up/1.01.sql
+++ mysql/up/1.01.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS `blog_simple`;
 CREATE TABLE `blog_simple` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `title` varchar(128) NOT NULL,
-  `content` text,
+  `entry` text,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
